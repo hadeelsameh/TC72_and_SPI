6857910bb9c2343a24fa6dc3880b40868d8f8168
hadeelsameh <43917398+hadeelsameh@users.noreply.github.com>
2021-06-06 15:22:17 +0200

    commit

---
+++ GccApplication7/GccApplication7/Debug/TC72.d
@@ -0,0 +1,25 @@
+TC72.d TC72.o: .././TC72.c .././TC72.h .././SPI.h .././TC72_cfg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././TC72.h:
+
+.././SPI.h:
+
+.././TC72_cfg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ GccApplication7/GccApplication7/main.c
@@ -0,0 +1,26 @@
+/*
+ * GccApplication7.c
+ *
+ * Created: 01/06/2021 11:40:07
+ * Author : Hadeel
+ */ 
+
+#include "TC72.h"
+#include "SPI.h"
+#include <util/delay.h>
+int main(void);
+
+int main(void)
+{
+ 
+	
+	tc72Init();
+	_delay_ms(150);
+	char MSB=0;
+	char LSB =0;
+    while (1) 
+    {
+		tc72_Read(&MSB ,&LSB);
+	}
+}
+
